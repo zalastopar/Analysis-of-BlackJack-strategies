decfeff0cc10a551ac022224c42bf3feb6293942
zalastopar <zala.stopar1@gmail.com>
2022-05-08 19:28:03 +0200

    functions for saving data

--- functions/save_data.py
+++ functions/save_data.py
@@ -4,22 +4,13 @@ import pandas as pd
 import os.path
 
 def save_simulation(hand, game):
-    '''
-    game
-        self.sim = sim # number of simulation
-        self.data_balance = data_balance # dictionary: key = a number of simulation, value = list of balances after each dealings
-        self.data_bet = data_bet # dictionary: key = a number of simulation, value = list of bets
-        self.data_0x = data_0x # list: 1 if balcance gets to 0, 0 else
-        self.data_3x = data_3x # list: 1 if balance gets to 3x, 0 else
-        self.data_5x = data_5x # list: 1 if balance gets to 5x, 0 else
-        self.data_10x = data_10x # list: 1 if balance gets to 10x, 0 else
-        self.data_to0 = data_to0
-    '''
+
     sim = game.sim
 
     # balance 
     if sim not in game.data_balance.keys():
         game.data_balance[sim] = [game.balance]
+    else:
         game.data_balance[sim].append(game.balance)
     
     # bet
@@ -27,6 +18,31 @@ def save_simulation(hand, game):
         game.data_bet[sim] = [hand.bet + hand.split_bet]
     else: 
         game.data_bet[sim].append(hand.bet + hand.split_bet)
+
+
+    #self.data_multiple = data_multiple
+    # data 0x
+    if sim not in game.data_multiple['data_0x'].keys():
+        if game.balance == 0: 
+            game.data_multiple['data_0x'][sim] = game.length
+
+    # data 3x
+    if sim not in game.data_multiple['data_3x'].keys(): # not yet to 300
+        if game.balance >= 300: # startin balance = 100
+            game.data_multiple['data_3x'][sim] = game.length
+
+    # data 5x
+    if sim not in game.data_multiple['data_5x'].keys(): # not yet to 500
+        if game.balance >= 500: # startin balance = 100
+            game.data_multiple['data_5x'][sim] = game.length
+
+    # data 0x
+    if sim not in game.data_multiple['data_10x'].keys(): # not yet to 1000
+        if game.balance >= 1000: # startin balance = 100
+            game.data_multiple['data_10x'][sim] = game.length
+
+
+    
 
     
 def save_prob(hand, game):
@@ -198,9 +214,9 @@ def update_csv_prob(new_dict, csv_file):
     # save as csv
     df = pd.DataFrame(updated)
     df.to_csv(csv_file)
-    print('old')
-    print(old_dict)
-    print('new')
-    print(new_dict)
-    print('combined')
-    print(updated)
+    #print('old')
+    #print(old_dict)
+    #print('new')
+    #print(new_dict)
+    #print('combined')
+    #print(updated)
